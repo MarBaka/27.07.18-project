ba65abe68f0cb8164bdff7c4ed8a209d3026f4ec
Ramilia Imankulova <ramiliaimankulova@Ramilias-Mac.local>
2018-07-27 11:09:02 -0700

    my storyboard

---
+++ 27.07.18 project/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  27.07.18 project
+//
+//  Created by Ramilia Imankulova on 7/27/18.
+//  Copyright © 2018 Marat. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
